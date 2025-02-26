df1f36614d0df0713ad43c817e1ec00a92cc1570
Nicholas Riegel <nicholaspriegel@gmail.com>
2025-02-26 18:44:06 -0500

    finished 1.1

--- part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -5,6 +5,14 @@ public class LeapYear {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        System.out.println("Give a year:");
+        int answer = Integer.valueOf(scan.nextLine());
+        if (answer % 4 != 0){
+            System.out.println("The year is not a leap year.");
+        } else if (answer % 100 == 0 && answer % 400 != 0){
+            System.out.println("The year is not a leap year.");
+        } else {
+            System.out.println("The year is a leap year.");
+        }
     }
 }
--- part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -5,6 +5,28 @@ public class GiftTax {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Value of gift?");
+        double answer = Double.valueOf(scan.nextLine());
+        double tax = 0;
+        if (answer >= 1000000){
+            tax = 142100 + (answer - 1000000) * .17;
+        } else if (answer >= 200000){
+            tax = 22100 + (answer - 200000) * .15;
+        } else if (answer >= 55000){
+            tax = 4700 + (answer - 55000) * .12;
+        } else if (answer >= 25000){
+            tax = 1700 + (answer - 25000) * .10;
+        } else if (answer >= 5000){
+            tax = 100 + (answer - 5000) * .08;
+        } else {
+            tax = 0;
+        }
 
+        if (tax == 0){
+            System.out.println("No Tax!");
+        } else {
+            System.out.println("Tax: "+tax);
+        }
+        
     }
 }
