8c1fedcf7f8e3ab8d3a9f5129266c6102b6da265
Nicholas Riegel <nicholaspriegel@gmail.com>
2025-03-04 20:12:12 -0500

    .

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,26 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+
+        Statistics stats = new Statistics();
+        Statistics evens = new Statistics();
+        Statistics odds = new Statistics();
+
+        System.out.println("Enter numbers:");
+        while (true) {
+            int answer = Integer.valueOf(scanner.nextLine());
+            if (answer == -1){
+                break;
+            }
+            stats.addNumber(answer);
+            if (answer % 2 == 0){
+                evens.addNumber(answer);
+            } else {
+                odds.addNumber(answer);
+            }
+        }
+        System.out.println("Sum: "+stats.sum());
+        System.out.println("Sum of even numbers: "+evens.sum());
+        System.out.println("Sum of odd numbers: "+odds.sum());
     }
 }
