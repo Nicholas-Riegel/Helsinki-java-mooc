2a37f1534dc4795a29b83659a44dcbd08c8c4280
Nicholas Riegel <nicholaspriegel@gmail.com>
2025-03-10 20:15:23 -0400

    .

--- part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
+++ part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
@@ -11,6 +11,33 @@ public class SimpleDate {
         this.year = year;
     }
 
+    // assuming only 30 days in a month
+    public void advance(){
+        if (this.day == 30 && this.month == 12){
+            this.day = 1;
+            this.month = 1;
+            this.year++;
+        } else if (this.day == 30){
+            this.day = 1;
+            this.month++;
+        } else {
+            this.day++;
+        }
+    }
+
+    public void advance(int howManyDays){
+        while(howManyDays > 0){
+            this.advance();
+            howManyDays--;
+        }
+    }
+
+    public SimpleDate afterNumberOfDays(int days){
+        SimpleDate newDateObject = new SimpleDate(this.day, this.month, this.year);
+        newDateObject.advance(days);
+        return newDateObject;
+    }
+
     @Override
     public String toString() {
         return this.day + "." + this.month + "." + this.year;
